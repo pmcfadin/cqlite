fe23af38fc762fd4dcb924f0877b8aa59eec75af
pmcfadin <pmcfadin@users.noreply.github.com>
2026-06-02 14:31:58 +0000

    deploy: 8b1bfa154f451dfc53af48fe99b5985f5e434166

--- latest/cqlite_core/storage/write_engine/sidebar-items.js
+++ latest/cqlite_core/storage/write_engine/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["cql_to_mutation","export","memtable","merge","merge_policy","mutation","wal"],"struct":["CompactionStats","MaintenanceReport","WriteEngine","WriteEngineConfig"],"trait":["MergePolicy"]};
+window.SIDEBAR_ITEMS = {"enum":["Durability"],"mod":["cql_to_mutation","export","memtable","merge","merge_policy","mutation","wal"],"struct":["CompactionStats","MaintenanceReport","WriteEngine","WriteEngineConfig"],"trait":["MergePolicy"]};
